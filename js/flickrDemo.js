0fda31170ab21ea64560e4a152069f89a6923a26
Noah Sloan <iamnoah+github@gmail.com>
2009-12-10 16:04:29 -0600

    updated demo

--- js/flickrDemo.js
+++ js/flickrDemo.js
@@ -1,17 +1,16 @@
 $(function() {
 	var dialog = $('#dialog'), originalHtml = dialog.html();
+	// ?sanitize=false toggles writeCapture()
+	if(!/sanitize=false/.test(window.location)) {
+		dialog = dialog.writeCapture();
+	}
 	$('.openDialog').click(function() {
 		dialog.dialog({width: 387, height: 415}).addClass('loading').
 			html(originalHtml).dialog('open');	
 		setTimeout(function() {
-			$.get('flickerPhotos.html',function(text) {				
-				dialog.removeClass('loading');
-				if(/sanitize=false/.exec(window.location)) {
-					dialog.html(text);
-				} else {
-					dialog.html(writeCapture.sanitize(text));
-				}
-			});		
+			dialog.load('flickerPhotos.html',function callback() {
+				this.removeClass('loading');
+			});
 		},1500); // pretend this call takes some time
 		return false;
 	});
