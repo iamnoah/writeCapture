17b6fdc59b77668d3d3fac8fb1430c42bfa48465
noah <iamnoah@gmail.com>
2010-06-01 19:09:58 -0400

    ensure queue always resumes

--- writeCapture.js
+++ writeCapture.js
@@ -141,7 +141,9 @@
 		if(!this.running) {
 			this.running = true;
 			try {
-				while(!this.paused && this._next());
+				// just in case there is a bug, always resume 
+				// if paused is less than 1
+				while(this.paused < 1 && this._next());
 			} finally {
 				this.running = false;
 			}
